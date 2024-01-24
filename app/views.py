9b772b0f27714edd94cd40a5a6936f12442fe40a
deepa28li <swaralipatil2000@gmail.com>
2024-01-24 10:25:27 +0530

    fisrt commit

--- app/views.py
+++ app/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login
+from django.urls import reverse
 
-def registation(request):
+def registration(request):
     ufo=UserForm()
     pfo=ProfileForm()
     d={'ufo':ufo,'pfo':pfo}
@@ -26,7 +28,30 @@ def registation(request):
                       'deepalipatil2800@gmail.com',
                       [MUFDO.email],fail_silently=True)
             
-            return HttpResponse('Registation successfull')
+            return HttpResponse('Registration successfull')
         else:
             return HttpResponse('Invalid data')
-    return render(request,'registation.html',d)
+    return render(request,'registration.html',d)
+
+
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            HttpResponse('Invalid credentials')
+    return render(request,'user_login.html')
